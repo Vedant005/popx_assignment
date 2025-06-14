fdf5d1991d90ae4720918550ca05239e0881b047
Vedant005 <vedantkanekar909@gmail.com>
2025-06-15 00:06:09 +0530

    feat: add routes for Signin, CreateAccount, and AccountSettings components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./Welcome";
+import Signin from "./Signin";
+import CreateAccount from "./CreateAccount";
+import AccountSettings from "./AccountSettings";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/create" element={<CreateAccount />} />
+        <Route path="/settings" element={<AccountSettings />} />
       </Routes>
     </>
   );
